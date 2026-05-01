105ee9032cc79f46702fe7362687f31ad80f40bd
TJBFW EVENT <drakew385@gmail.com>
2026-05-01 19:52:23 +0000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WilfriedPortfolio from './wilfried-portfolio';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <WilfriedPortfolio />
+  </React.StrictMode>
+);
